74074c3341b2f9317a0c38783b4d61c4683d632f
Thomas Darimont <thomas.darimont@gmail.com>
2017-02-02 23:09:48 +0100

    Add missing ScreenCastWebApi

---
+++ src/main/java/de/tdlabs/apps/screencast/ScreenCastWebApi.java
@@ -0,0 +1,18 @@
+package de.tdlabs.apps.screencast;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+class ScreenCastWebApi {
+
+  private final ScreenFetcher screenFetcher;
+
+  @GetMapping(path = "/screenshot", produces = MediaType.IMAGE_JPEG_VALUE)
+  byte[] getScreenshot() {
+    return screenFetcher.getCurrentImage();
+  }
+}

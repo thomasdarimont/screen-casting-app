53c282dc09209e90dd0da08e70ac1933d4f5addb
Thomas Darimont <thomas.darimont@gmail.com>
2018-03-12 19:14:28 +0100

    Rename SecurityConfig to WebSecurityConfig

--- src/main/java/de/tdlabs/apps/screencaster/config/WebSecurityConfig.java
+++ src/main/java/de/tdlabs/apps/screencaster/config/WebSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {

7f0b0d7e143322dcf75f7b6fb9796fa2134ee63d
Thomas Darimont <thomas.darimont@gmail.com>
2018-08-12 18:32:59 +0200

    Adapt WebSocket configuration to spring-boot 2.x

--- src/main/java/de/tdlabs/apps/screencaster/config/WebSocketConfig.java
+++ src/main/java/de/tdlabs/apps/screencaster/config/WebSocketConfig.java
@@ -2,9 +2,9 @@ package de.tdlabs.apps.screencaster.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import static de.tdlabs.apps.screencaster.config.WebsocketDestinations.TOPIC_NOTES;
 import static de.tdlabs.apps.screencaster.config.WebsocketDestinations.TOPIC_POINTER;
@@ -12,7 +12,7 @@ import static de.tdlabs.apps.screencaster.config.WebsocketDestinations.TOPIC_SET
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {

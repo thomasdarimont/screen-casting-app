18d3529298a346a31c4a2327a999383745b34f60
Thomas Darimont <thomas.darimont@gmail.com>
2018-08-15 21:18:15 +0200

    Revise access control in AccessGuard

--- src/main/java/de/tdlabs/apps/screencaster/security/AccessGuard.java
+++ src/main/java/de/tdlabs/apps/screencaster/security/AccessGuard.java
@@ -1,10 +1,12 @@
 package de.tdlabs.apps.screencaster.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AccessGuard {
@@ -12,6 +14,14 @@ public class AccessGuard {
   private final HttpServletRequest currentRequest;
 
   public boolean isStreamerRequest() {
-    return currentRequest.getRemoteAddr().equals(currentRequest.getLocalAddr());
+    String remoteAddr = currentRequest.getRemoteAddr();
+    String localAddr = currentRequest.getLocalAddr();
+
+    if (remoteAddr.equals(localAddr)) {
+      return true;
+    }
+
+    log.warn("Access denied. remoteAddr=%s localAddr=%s%n", remoteAddr, localAddr);
+    return false;
   }
 }

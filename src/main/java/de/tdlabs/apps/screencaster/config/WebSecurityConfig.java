cd445b08fd45a9b2870ea9a9ddab2cce087671f1
Thomas Darimont <thomas.darimont@googlemail.com>
2020-03-17 11:10:50 +0100

    Revise spring security configuration

--- src/main/java/de/tdlabs/apps/screencaster/config/WebSecurityConfig.java
+++ src/main/java/de/tdlabs/apps/screencaster/config/WebSecurityConfig.java
@@ -1,8 +1,13 @@
 package de.tdlabs.apps.screencaster.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Collections;
 
 @Configuration
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,9 +19,13 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .authorizeRequests()
       .anyRequest().permitAll()
       .and().headers().frameOptions().deny()
-//      .and()
-//      .formLogin().and()
-//      .httpBasic()
+      .and().formLogin().disable()
+      .httpBasic().disable()
     ;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return new InMemoryUserDetailsManager(Collections.emptyList());
   }
 }
